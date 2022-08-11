b0e142ccca8c48a1d335e6745007a304168ca523
jiaweizhong <oracleinscience@gmail.com>
2022-08-10 20:26:23 -0700

    Initial commit

--- lane.py
+++ lane.py
@@ -1,15 +1,25 @@
 import cv2
 import numpy as np
 
+def canny(image):
+    # convert to grayscale (3 channel -> 1 channel)
+    gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
+    # reduce noise with Gaussian filter (avoid false edge)
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)
+    # compute gradient to detect edges
+    canny = cv2.Canny(blur, 50, 150)
+    return canny
+
+def region_of_interest(image):
+    height = image.shape[0]
+    polygons = np.array([[(200, height), (1100, height), (550, 250)]])
+    mask = np.zeros_like(image)
+    cv2.fillPoly(mask, polygons, 255)
+    return mask
+
 image = cv2.imread('test_image.jpg')
 # avoid modification to original image
 lane_image = np.copy(image)
-# convert to grayscale (3 channel -> 1 channel)
-gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
-# reduce noise with Gaussian filter (avoid false edge)
-blur = cv2.GaussianBlur(gray, (5,5), 0)
-# compute gradient to detect edges
-canny = cv2.Canny(blur, 50, 150)
-
-cv2.imshow('result', canny)
+canny = canny(lane_image)
+cv2.imshow("result", region_of_interest(canny))
 cv2.waitKey(0)
